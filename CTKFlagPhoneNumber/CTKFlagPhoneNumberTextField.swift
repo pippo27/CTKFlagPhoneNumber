19ed3c56b6b867d526bbd55320ed575164fe1cd7
Zryte <filmmanuchet@gmail.com>
2019-06-12 17:48:35 +0700

    update to swift 4

--- CTKFlagPhoneNumber/CTKFlagPhoneNumberTextField.swift
+++ CTKFlagPhoneNumber/CTKFlagPhoneNumberTextField.swift
@@ -99,7 +99,7 @@ open class CTKFlagPhoneNumberTextField: UITextField, UITextFieldDelegate, Countr
     
 	private func setup() {
 		borderStyle = .roundedRect
-		leftViewMode = UITextFieldViewMode.always
+		leftViewMode = UITextField.ViewMode.always
 		autocorrectionType = .no
 		addTarget(self, action: #selector(didEditText), for: .editingChanged)
 		delegate = self
@@ -269,13 +269,13 @@ open class CTKFlagPhoneNumberTextField: UITextField, UITextFieldDelegate, Countr
 	}
 
 	private func getCountryListBarButtonItems() -> [UIBarButtonItem] {
-		let space = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
-		let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(resetKeyBoard))
+		let space = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+		let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.done, target: self, action: #selector(resetKeyBoard))
 		
 		doneButton.accessibilityLabel = "doneButton"
 		
 		if parentViewController != nil {
-			let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.search, target: self, action: #selector(displayAlphabeticKeyBoard))
+			let searchButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.search, target: self, action: #selector(displayAlphabeticKeyBoard))
 			
 			searchButton.accessibilityLabel = "searchButton"
 			
